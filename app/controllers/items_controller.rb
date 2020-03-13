6a195904245543db90ad7b2fa0332819b0fd21fe
nakaoyuka <58930395+nakaoyuka@users.noreply.github.com>
2020-03-13 16:41:17 +0900

    nakao-productpage1ブランチをMerge into Current Branch・itemsコントローラのアクション、ルーティング設定、view,scssファイル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
   def show
   end
+
+  def confirm
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root 'tops#index'
   get 'items/show', to: 'items#show'
+  # ↓商品購入確認ページ ルーティング
+  get 'items/confirm', to: 'items#confirm'
   # resources :items, only: :show
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
