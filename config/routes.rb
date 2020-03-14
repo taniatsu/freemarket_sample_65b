dd986ae194830a060d978e029955f9058d874e72
Taka0010 <takayuki.yamato@gmail.com>
2020-03-14 12:58:08 +0900

    いいね一覧
    ・いいね一覧のviewを作りました。
    ・部分テンプレート
    ・reset.css一部修正
    ・likes controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'tops#index'
-  
+  resources :users do
+    resource :self_address
+  end
+  resources :items do
+    resources :likes
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
