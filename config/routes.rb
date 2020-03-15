e1f7691bd2d57479c17b5d2d40bc770111f3dbcc
Taka0010 <takayuki.yamato@gmail.com>
2020-03-15 17:05:19 +0900

    ベースルーティング
    ベースルーティング記述しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
-  
+  # ルート指定
   root 'tops#index'
-  # [confirm]は商品購入確認ページのルーティング
+
+  # ユーザーページのルーティング
+  resources :users do
+    resource :self_address
+    resources :delivery_addresses
+  end
+
+  # 商品ページのルーティング
   resources :items do
+    resources :likes
+    resources :comments
     collection do
       get 'confirm'
     end
   end
+
+  # カード登録ページのルーティング
+  resources :cards
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
