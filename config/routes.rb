2643b3bb7851c695969c794f3d7c3baba20341a6
nakaoyuka <58930395+nakaoyuka@users.noreply.github.com>
2020-03-15 15:32:02 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   
   root 'tops#index'
-  get 'items/show', to: 'items#show'
-  # ↓商品購入確認ページ ルーティング
-  get 'items/confirm', to: 'items#confirm'
-  # resources :items, only: :show
+  # [confirm]は商品購入確認ページのルーティング
+  resources :items, only: [:show, :confirm]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
