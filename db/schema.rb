da588e8578863281773d2a0638b49e01657c2d05
ribstar <wood.sk.love315@icloud.com>
2020-04-12 23:41:16 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200328053809) do
+ActiveRecord::Schema.define(version: 20200408110354) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 20200328053809) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sns_credentials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                            null: false
     t.string   "email",                               null: false
@@ -62,16 +71,10 @@ ActiveRecord::Schema.define(version: 20200328053809) do
     t.string   "first_name",                          null: false
     t.string   "last_jp_name",                        null: false
     t.string   "first_jp_name",                       null: false
-    t.integer  "year",                                null: false
-    t.date     "month",                               null: false
-    t.date     "day",                                 null: false
-    t.integer  "tel"
-    t.string   "zipcode",                             null: false
-    t.string   "prefecture",                          null: false
-    t.string   "city",                                null: false
-    t.string   "address",                             null: false
-    t.string   "building"
-    t.string   "tel_num"
+    t.integer  "year_id",                             null: false
+    t.integer  "month_id",                            null: false
+    t.integer  "day_id",                              null: false
+    t.string   "tel"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -89,4 +92,5 @@ ActiveRecord::Schema.define(version: 20200328053809) do
   end
 
   add_foreign_key "images", "items"
+  add_foreign_key "sns_credentials", "users"
 end
