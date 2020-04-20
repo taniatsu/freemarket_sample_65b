f6e25f8bc919f303890a030408484efc6d7cec8a
Taka0010 <takayuki.yamato@gmail.com>
2020-04-20 22:40:36 +0900

    単体テスト
    単体テスト

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    user_id                {"1"}
+    customer_id             {"2"}
+    card_id              {"testtest0010"}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#create' do
+    it "use_idがない場合は登録できないこと" do
+      card = build(:card, user_id: nil)
+      card.valid?
+      expect(card.errors[:user_id]).to include("がありません")
+    end
+
+    it "use_idがない場合は登録できないこと" do
+      card = build(:card, customer_id: nil)
+      card.valid?
+      expect(card.errors[:customer_id]).to include("がありません")
+    end
+
+    it "use_idがない場合は登録できないこと" do
+      card = build(:card, card_id: nil)
+      card.valid?
+      expect(card.errors[:card_id]).to include("がありません")
+    end
+
+  end
 end
