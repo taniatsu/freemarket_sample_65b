9ac40ab8c8bcf39bb5f8f0afd1e87232d0af4eae
taniatsu <mjp569.gj3@gmail.com>
2020-05-03 10:02:07 +0900

    一覧ピックアップ非表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,7 @@
 class TopsController < ApplicationController
   def index
     @items = Item.all.order(id: "DESC")
-    @lastItem = @items.first(3)
+    @lastItem = @items.first(6)
     @parents = Category.all.where(ancestry: nil)
   end
 end
