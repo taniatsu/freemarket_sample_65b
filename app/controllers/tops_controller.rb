36ed4125f451c0252d927ecb9cf40d3800f89229
たにがき <58585061+taniatsu@users.noreply.github.com>
2020-05-03 13:49:24 +0900

    Revert " 商品一覧・詳細修正"

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,7 @@
 class TopsController < ApplicationController
   def index
     @items = Item.all.order(id: "DESC")
-    @lastItem = @items.first(6)
+    @lastItem = @items.first(3)
     @parents = Category.all.where(ancestry: nil)
   end
 end
